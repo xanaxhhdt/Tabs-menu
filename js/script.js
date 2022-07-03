708d12c3ba46f50970e7b6dfcc5cc5ab2cf36fab
vladislav <nicochan00@mail.ru>
2022-07-04 00:46:28 +0300

    markup + tab styles

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const tab = function () {
+   let tabNav = document.querySelectorAll('.tabs-nav__item'),
+      tabContent = document.querySelectorAll('.tab'),
+      tabName;
+
+   tabNav.forEach(item => {
+      item.addEventListener('click', selectTavNav);
+   });
+
+   function selectTavNav() {
+      console.log(this);
+   }
+};
+
+tab();
